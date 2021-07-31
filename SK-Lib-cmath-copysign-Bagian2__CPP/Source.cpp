1465d6935ec5adcd501322c8d4b87c239754115d
Rizky Khapidsyah <rizkykhapidsyah@gmail.com>
2021-07-31 20:26:08 +0700

    Add project files.

---
+++ SK-Lib-cmath-copysign-Bagian2__CPP/Source.cpp
@@ -0,0 +1,25 @@
+
+#include <conio.h>
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+/*
+	Source by GeeksForGeeks
+	Modified For Learn by RK
+	I.D.E : VS2019
+*/
+
+int main() {
+	cout << "Magnitude = 9 Sign = -3 " << endl;
+	cout << "Copysign(9, -3) = " << copysign(9, -3) << endl;
+
+	cout << endl;
+
+	cout << "Magnitude = -67 Sign = 4 " << endl;
+	cout << "Copysign(-67, 4) = " << copysign(-67, 4);
+
+	_getch();
+	return 0;
+}
